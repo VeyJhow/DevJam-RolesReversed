1c16038c219312bb160d91b7b8d3d00fb000a483
Samscuckoo <samuelcanellasf@gmail.com>
2024-03-24 01:26:28 -0300

    [UPD]
    Trabalhando mais no randomizador de cenas

--- SceneRand.gd
+++ SceneRand.gd
@@ -1,5 +1,12 @@
 extends Node2D
 
+var packed_scene = [
+	#coloca aqui as cenas que podem ser carregadas
+]
+
 func _scene_randomizer():
 	randomize()
-	var x = randi_range()
+	#randomiza uma cena do vetor de cenas
+	var x = randi_range(0, packed_scene.size())
+	var scene = packed_scene[x].instance()
+	get_tree().change_scene_to_file(x)
